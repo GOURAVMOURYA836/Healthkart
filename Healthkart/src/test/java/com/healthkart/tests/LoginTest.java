5fb820fc2786aa3e68ca7dd4886358363a2284c0
gourav <gouravmourya836@gmail.com>
2023-03-02 02:38:09 +0530

    First commit

---
+++ Healthkart/src/test/java/com/healthkart/tests/LoginTest.java
@@ -0,0 +1,38 @@
+package com.healthkart.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+public static String  P="dsfasf49s8f5s648";
+	
+public  int method4(int x)
+{
+	return x;
+}
+
+
+
+	@Test(priority=10,enabled=true)
+	public void m1() {
+		System.out.println(method4(10));
+	int x=999*8/7+96-980+10000000;	
+		System.out.println(10/0);
+		
+		
+		
+	}
+	
+	
+	@Test(priority=1)
+	public void verifyUserLogin() {
+		
+		
+		System.out.println("i am method login");
+		
+		
+		
+	}
+	
+	
+
+}
